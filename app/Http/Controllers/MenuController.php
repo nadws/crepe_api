af2e3903ceadc9981e3ee2380e954e59e6f73ff0
aldmf26 <aldimf26@gmail.com>
2023-08-24 14:02:39 +0800

    123

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -21,7 +21,7 @@ class MenuController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    
+
     public function index(Request $request)
     {
 
@@ -33,23 +33,23 @@ class MenuController extends Controller
         } else {
             if (Auth::user()->jenis == 'adm') {
                 $id_lokasi = $request->id_lokasi;
-                if( $id_lokasi == '1'){
+                if ($id_lokasi == '1') {
                     $lokasi = 'TAKEMORI';
-                }else{
+                } else {
                     $lokasi = 'SOONDOBU';
                 }
-                
+
                 $data = [
                     'title' => 'Menu',
                     'logout' => $request->session()->get('logout'),
                     'menu1' => DB::raw("SELECT tb_menu.*, tb_kategori.* FROM tb_menu LEFT JOIN tb_kategori ON tb_menu.id_kategori = tb_kategori.kd_kategori WHERE tb_menu.lokasi = $id_lokasi "),
                     'menu' => DB::table('mHandicap')->join('tb_station', 'tb_station.id_station', 'mHandicap.id_station')->where('mHandicap.lokasiMenu', $request->id_lokasi)->get(),
 
-                    'kategori' => DB::table('tb_kategori')->where('lokasi',$lokasi)->get(),
+                    'kategori' => DB::table('tb_kategori')->where('lokasi', $lokasi)->get(),
                     'distribusi' => Distribusi::all(),
-                    'handicap' => Handicap::where('id_lokasi',$id_lokasi)->get(),
+                    'handicap' => Handicap::where('id_lokasi', $id_lokasi)->get(),
                     'id_lokasi' => $id_lokasi,
-                //   'menu' => DB::table('mHandicap')->join('tb_station', 'tb_station.id_station', 'mHandicap.id_station')->where('mHandicap.lokasiMenu', $request->id_lokasi)->get(),
+                    //   'menu' => DB::table('mHandicap')->join('tb_station', 'tb_station.id_station', 'mHandicap.id_station')->where('mHandicap.lokasiMenu', $request->id_lokasi)->get(),
                 ];
 
                 return view("menu.menu", $data);
@@ -62,15 +62,15 @@ class MenuController extends Controller
     public function tblMenu(Request $request)
     {
         $data = [
-//             'menu' => DB::select("SELECT * FROM `mHandicap` as a
-// LEFT JOIN tb_station as b on a.id_station = b.id_station
-// WHERE a.lokasiMenu = $request->id_lokasi"),
+            //             'menu' => DB::select("SELECT * FROM `mHandicap` as a
+            // LEFT JOIN tb_station as b on a.id_station = b.id_station
+            // WHERE a.lokasiMenu = $request->id_lokasi"),
             'menu' => DB::table('mHandicap')->join('tb_station', 'tb_station.id_station', 'mHandicap.id_station')->where('mHandicap.lokasiMenu', $request->id_lokasi)->paginate(10),
             'menu1' => DB::table('tb_menu')->select('tb_menu.*', 'tb_kategori.*')->join('tb_kategori', 'tb_menu.id_kategori', '=', 'tb_kategori.kd_kategori')->where('tb_menu.lokasi', $request->id_lokasi)->orderBy('tb_menu.id_menu', 'DESC')->paginate(10),
             'id_lokasi' => $request->id_lokasi,
             'keyword' => $request->keyword,
         ];
-        return view('menu.table',['page' => 1],$data);
+        return view('menu.table', ['page' => 1], $data);
     }
 
     public function cariMenu(request $request)
@@ -84,105 +84,103 @@ LEFT JOIN tb_handicap as d ON a.id_handicap = d.id_handicap
 WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_menu LIKE '%$keyword%'"),
             'id_lokasi' => $request->id_lokasi,
             'keyword' => $keyword,
-            
+
         ];
-        return view('menu.cari',$data);
+        return view('menu.cari', $data);
     }
 
     public function importMenu(Request $request)
     {
-            // include APPPATH.'third_party/PHPExcel/PHPExcel.php';
-            $file = $request->file('file');
-            $ext = $file->getClientOriginalExtension();
+        // include APPPATH.'third_party/PHPExcel/PHPExcel.php';
+        $file = $request->file('file');
+        $ext = $file->getClientOriginalExtension();
 
-            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-            $spreadsheet = $reader->load($file);
-            // $loadexcel = $excelreader->load('excel/'.$this->filename.'.xlsx'); // Load file yang telah diupload ke folder excel
-            // $sheet = $loadexcel->getActiveSheet()->toArray(null, true, true ,true);
-            $sheet = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
-            $lokasi = $request->id_lokasi;
-    
-    
-            $data = array();
-            $numrow = 1;
-            // cek
-            $cek = 0;
-            foreach ($sheet as $row) {
-    
-                if ($row['A'] == "" && $row['B'] == "" && $row['C'] == "" && $row['D'] == "" && $row['E'] == "" && $row['F'] == "")
-                    continue;
-                $numrow++; // Tambah 1 setiap kali looping
-            }
-            // endcek
-        
-            
-            $kmenu = Menu::orderBy('kd_menu', 'desc')->where('lokasi', $lokasi)->first();
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        $spreadsheet = $reader->load($file);
+        // $loadexcel = $excelreader->load('excel/'.$this->filename.'.xlsx'); // Load file yang telah diupload ke folder excel
+        // $sheet = $loadexcel->getActiveSheet()->toArray(null, true, true ,true);
+        $sheet = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        $lokasi = $request->id_lokasi;
 
-            $kd_menu = $kmenu->kd_menu + 1;
 
-            foreach ($sheet as $row) {
-                if ($row['A'] == "" && $row['B'] == "" && $row['C'] == "" && $row['D'] == "" && $row['E'] == "" && $row['F'] == "")
-                    continue;
-    
-    
-                if ($numrow > 1) {
-                    
-                    $data = [
-                        'id_kategori' => $row['A'],
-                        'kd_menu' => $kd_menu++,
-                        'nm_menu' => $row['C'],
-                        'tipe' => $row['D'],
-                        'aktif' => 'on',
-                        'lokasi' => $lokasi,
+        $data = array();
+        $numrow = 1;
+        // cek
+        $cek = 0;
+        foreach ($sheet as $row) {
+
+            if ($row['A'] == "" && $row['B'] == "" && $row['C'] == "" && $row['D'] == "" && $row['E'] == "" && $row['F'] == "")
+                continue;
+            $numrow++; // Tambah 1 setiap kali looping
+        }
+        // endcek
+
+
+        $kmenu = Menu::orderBy('kd_menu', 'desc')->where('lokasi', $lokasi)->first();
+
+        $kd_menu = $kmenu->kd_menu + 1;
+
+        foreach ($sheet as $row) {
+            if ($row['A'] == "" && $row['B'] == "" && $row['C'] == "" && $row['D'] == "" && $row['E'] == "" && $row['F'] == "")
+                continue;
+
+
+            if ($numrow > 1) {
+
+                $data = [
+                    'id_kategori' => $row['A'],
+                    'kd_menu' => $kd_menu++,
+                    'nm_menu' => $row['C'],
+                    'tipe' => $row['D'],
+                    'aktif' => 'on',
+                    'lokasi' => $lokasi,
+                ];
+                $menu = Menu::create($data);
+
+                if ($row['D'] == '') {
+                    # code...
+                } else {
+                    $data2 = [
+                        'id_menu' => $menu->id,
+                        'id_distribusi' => '1',
+                        'harga' => $row['E']
                     ];
-                    $menu = Menu::create($data);
-    
-                    if ($row['D'] == '') {
-                        # code...
-                    } else {
-                        $data2 = [
-                            'id_menu' => $menu->id,
-                            'id_distribusi' => '1',
-                            'harga' => $row['E']
-                        ];
-                        Harga::create($data2);
-                    }
-
-                    if ($row['E'] == '') {
-                        # code...
-                    } else {
-                        $data3 = [
-                            'id_menu' => $menu->id,
-                            'id_distribusi' => '3',
-                            'harga' => $row['F']
-                        ];
-                        Harga::create($data3);
-                    }
-    
-                    if ($row['F'] == '') {
-                        # code...
-                    } else {
-                        $data4 = [
-                            'id_menu' => $menu->id,
-                            'id_distribusi' => '2',
-                            'harga' => $row['G']
-                        ];
-                        Harga::create($data4);
-                    }
+                    Harga::create($data2);
                 }
-                $numrow++; // Tambah 1 setiap kali looping
-            }
-    
-            return redirect()->route('menu', ['id_lokasi' => 1])->with('sukses', 'Data berhasil Diimport');
 
-        
+                if ($row['E'] == '') {
+                    # code...
+                } else {
+                    $data3 = [
+                        'id_menu' => $menu->id,
+                        'id_distribusi' => '3',
+                        'harga' => $row['F']
+                    ];
+                    Harga::create($data3);
+                }
+
+                if ($row['F'] == '') {
+                    # code...
+                } else {
+                    $data4 = [
+                        'id_menu' => $menu->id,
+                        'id_distribusi' => '2',
+                        'harga' => $row['G']
+                    ];
+                    Harga::create($data4);
+                }
+            }
+            $numrow++; // Tambah 1 setiap kali looping
+        }
+
+        return redirect()->route('menu', ['id_lokasi' => 1])->with('sukses', 'Data berhasil Diimport');
     }
 
     public function exportMenu(Request $request)
     {
         $id_lokasi = $request->lokasi;
         $lokasiTs = $id_lokasi == 1 ? 'TAKEMORI' : 'SOONDOBU';
-        
+
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
         $sheet->getStyle('A1:D4')
@@ -213,62 +211,61 @@ WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_m
             ->setCellValue('N1', 'SOONDOBU')
             ->setCellValue('O1', 'KODE KATEGORI')
             ->setCellValue('P1', 'KATEGORI');
-        
+
         $tkm = DB::table('tb_kategori')->where('lokasi', "TAKEMORI")->get();
         $sdb = DB::table('tb_kategori')->where('lokasi', "SOONDOBU")->get();
         $tbMenu = DB::table('tb_menu')->select('tb_menu.*', 'tb_kategori.*')
-                        ->join('tb_kategori', 'tb_menu.id_kategori', '=', 'tb_kategori.kd_kategori')
-                        ->where('tb_menu.lokasi', $id_lokasi)->orderBy('tb_menu.id_menu', 'DESC')
-                        ->get();
+            ->join('tb_kategori', 'tb_menu.id_kategori', '=', 'tb_kategori.kd_kategori')
+            ->where('tb_menu.lokasi', $id_lokasi)->orderBy('tb_menu.id_menu', 'DESC')
+            ->get();
         $lom = 2;
-        foreach($tbMenu as $t) {
+        foreach ($tbMenu as $t) {
             $sheet
-                ->setCellValue('A'.$lom,$t->kd_kategori)
-                ->setCellValue('B'.$lom,$t->kategori)
-                ->setCellValue('C'.$lom,$t->nm_menu)
-                ->setCellValue('D'.$lom,$t->tipe);
-                
+                ->setCellValue('A' . $lom, $t->kd_kategori)
+                ->setCellValue('B' . $lom, $t->kategori)
+                ->setCellValue('C' . $lom, $t->nm_menu)
+                ->setCellValue('D' . $lom, $t->tipe);
+
             $h = DB::table('tb_harga')
-                    ->select('tb_harga.*', 'tb_distribusi.*')
-                    ->join('tb_distribusi', 'tb_harga.id_distribusi', '=', 'tb_distribusi.id_distribusi')
-                    ->where('id_menu', $t->id_menu)
-                    ->first();
-            if($h) {
-                $sheet->setCellValue('E'.$lom,$h->harga ? $h->harga : 0);
-                if($h->id_distribusi == 2) {
+                ->select('tb_harga.*', 'tb_distribusi.*')
+                ->join('tb_distribusi', 'tb_harga.id_distribusi', '=', 'tb_distribusi.id_distribusi')
+                ->where('id_menu', $t->id_menu)
+                ->first();
+            if ($h) {
+                $sheet->setCellValue('E' . $lom, $h->harga ? $h->harga : 0);
+                if ($h->id_distribusi == 2) {
                     $th = DB::table('tb_harga')
-                    ->select('tb_harga.*', 'tb_distribusi.*')
-                    ->join('tb_distribusi', 'tb_harga.id_distribusi', '=', 'tb_distribusi.id_distribusi')
-                    ->where('id_menu', $t->id_menu)
-                    ->where('id_distribusi', 2)
-                    ->first();
-                    if($th) {
-                        
+                        ->select('tb_harga.*', 'tb_distribusi.*')
+                        ->join('tb_distribusi', 'tb_harga.id_distribusi', '=', 'tb_distribusi.id_distribusi')
+                        ->where('id_menu', $t->id_menu)
+                        ->where('id_distribusi', 2)
+                        ->first();
+                    if ($th) {
                     }
-                    $sheet->setCellValue('F'.$lom,$h->harga ? $h->harga : 0);
+                    $sheet->setCellValue('F' . $lom, $h->harga ? $h->harga : 0);
                 }
-                $sheet->setCellValue('G'.$lom,$h->harga ? $h->harga : 0);
+                $sheet->setCellValue('G' . $lom, $h->harga ? $h->harga : 0);
             }
-            
-                
+
+
             $lom++;
         }
         $kolom = 2;
         foreach ($tkm as $k) {
             $sheet
-                ->setCellValue('J'.$kolom,$k->kd_kategori)
-                ->setCellValue('K'.$kolom,$k->kategori);
+                ->setCellValue('J' . $kolom, $k->kd_kategori)
+                ->setCellValue('K' . $kolom, $k->kategori);
             $kolom++;
         }
 
         $kol = 2;
         foreach ($sdb as $k) {
             $sheet
-                ->setCellValue('O'.$kol,$k->kd_kategori)
-                ->setCellValue('P'.$kol,$k->kategori);
+                ->setCellValue('O' . $kol, $k->kd_kategori)
+                ->setCellValue('P' . $kol, $k->kategori);
             $kol++;
         }
-        
+
         $writer = new Xlsx($spreadsheet);
         $style = [
             'borders' => [
@@ -284,11 +281,11 @@ WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_m
 
         // tambah style
         $batas1 = count($tbMenu) + 1;
-        $sheet->getStyle('A1:G'.$batas1)->applyFromArray($style);
+        $sheet->getStyle('A1:G' . $batas1)->applyFromArray($style);
         $batas = count($tkm) + 1;
-        $sheet->getStyle('J1:K'.$batas)->applyFromArray($style);
+        $sheet->getStyle('J1:K' . $batas)->applyFromArray($style);
         $batas = count($sdb) + 1;
-        $sheet->getStyle('O1:P'.$batas)->applyFromArray($style);
+        $sheet->getStyle('O1:P' . $batas)->applyFromArray($style);
         // center
         $sheet->getStyle('I1')->getAlignment()->setHorizontal('center');
         // $sheet->getStyle('M1')->getAlignment()->setHorizontal('center');
@@ -307,10 +304,10 @@ WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_m
 
         $kd_menu = $menu->kd_menu + 1;
 
-        if($request->hasFile('image')){
+        if ($request->hasFile('image')) {
             $request->file('image')->move('assets/tb_menu/', $request->file('image')->getClientOriginalName());
-        $foto = $request->file('image')->getClientOriginalName();
-        }else{
+            $foto = $request->file('image')->getClientOriginalName();
+        } else {
             $foto = '';
         }
 
@@ -368,7 +365,7 @@ WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_m
             'aktif' => 'on',
 
         ];
-        
+
         $menu = Menu::where('id_menu', $request->id_menu)->update($data1);
         $id_menu = $request->id_menu;
         $id_distribusi = $request->id_distribusi;
@@ -416,11 +413,11 @@ WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_m
             return back()->with('error', 'Distribusi sudah ada');
         }
     }
-    
+
     public function tbhKategori(Request $request)
     {
         $lokasi = $request->lokasi == 1 ? 'TAKEMORI' : 'SOONDOBU';
-   
+
         $data = [
             'kd_kategori' => $request->kd_kategori,
             'kategori' => $request->nm_kategori,
@@ -429,14 +426,14 @@ WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_m
         Kategori::create($data);
         return redirect()->route('menu', ['id_lokasi' => $request->lokasi])->with('sukses', 'Berhasil Tambah Kategori');
     }
-    
-    public function station(Request $r) 
+
+    public function station(Request $r)
     {
         $data = [
             'id_lokasi' => $r->id_lokasi
         ];
-      
-        return view('menu.station',$data);
+
+        return view('menu.station', $data);
     }
     public function addStation(Request $r)
     {
@@ -444,29 +441,27 @@ WHERE a.lokasi = '$request->id_lokasi' AND a.nm_menu LIKE '%$keyword%' OR a.kd_m
             'nm_station' => $r->nm_station,
             'id_lokasi' => $r->id_lokasi,
         ]);
-
-        
     }
 
     public function delStation(Request $r)
     {
         DB::table('tb_station')->where('id_station', $r->id_station)->delete();
     }
-    
+
     public function editMenu(Request $r)
     {
         $data = [
             'id_menu' => $r->id_menu,
-            'menu' => DB::table('tb_menu')->select('tb_menu.*', 'tb_kategori.*')->join('tb_kategori', 'tb_menu.id_kategori', '=', 'tb_kategori.kd_kategori')->where('tb_menu.id_menu', $r->id_menu  )->orderBy('tb_menu.id_menu', 'DESC')->first(),
+            'menu' => DB::table('tb_menu')->select('tb_menu.*', 'tb_kategori.*')->join('tb_kategori', 'tb_menu.id_kategori', '=', 'tb_kategori.kd_kategori')->where('tb_menu.id_menu', $r->id_menu)->orderBy('tb_menu.id_menu', 'DESC')->first(),
             'kategori' => DB::table('tb_kategori')->get(),
-                    'distribusi' => Distribusi::all(),
-                    'lokasi' => $r->id_lokasi,
-                    'id_lokasi' => $r->id_lokasi,
-                    'keyword' => $r->keyword,
-   
-                    'handicap' => Handicap::where('id_lokasi',$r->id_lokasi)->get(),
-                    // 'bahan' => DB::table('tb_list_bahan')->where([['id_lokasi', $r->id_lokasi],['jenis',1]])->get(),
-                    'satuan' => DB::table('tb_satuan')->whereIn('id_satuan', [12,18,22,24,25,26])->get()
+            'distribusi' => Distribusi::all(),
+            'lokasi' => $r->id_lokasi,
+            'id_lokasi' => $r->id_lokasi,
+            'keyword' => $r->keyword,
+
+            'handicap' => Handicap::where('id_lokasi', $r->id_lokasi)->get(),
+            // 'bahan' => DB::table('tb_list_bahan')->where([['id_lokasi', $r->id_lokasi],['jenis',1]])->get(),
+            'satuan' => DB::table('tb_satuan')->whereIn('id_satuan', [12, 18, 22, 24, 25, 26])->get()
         ];
         return view('menu.editMenu', $data);
     }
