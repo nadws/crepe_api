c666652a6d804555cf732154e75e82cc78e935a7
nadws <nandw567@gmail.com>
2023-12-07 10:03:07 +0800

    das

--- app/Models/ApiInvoiceModel.php
+++ app/Models/ApiInvoiceModel.php
@@ -26,7 +26,7 @@ class ApiInvoiceModel extends Model
         round((a.total_orderan + if(b.bayar is null,0,b.bayar) - a.voucher + a.service + a.tax) - ( (a.total_orderan + if(b.bayar is null,0,b.bayar) - a.voucher + a.service + a.tax) *  (a.discount / 100)),0 ) as ttl_sebelum_round,
         sum(a.total_bayar) as total_bayar,
         sum(round((a.total_bayar + a.dp) - ((a.total_orderan + if(b.bayar is null,0,b.bayar) - a.voucher + a.service + a.tax) - ( (a.total_orderan + if(b.bayar is null,0,b.bayar) - a.voucher + a.service + a.tax) *  (a.discount / 100))),0)) as rounding,
-        sum(a.cash) as cash, sum(a.d_bca) as d_bca, sum(a.k_bca) as k_bca, sum(a.d_mandiri) as d_mandiri, sum(a.k_mandiri) as k_mandiri, sum(a.d_bri) as d_bri, sum(a.k_bri) as k_bri,
+        sum(a.cash) as cash, sum(a.d_bca) as d_bca, sum(a.k_bca) as k_bca, sum(a.d_mandiri) as d_mandiri, sum(a.k_mandiri) as k_mandiri, sum(a.d_bri) as d_bri, sum(a.k_bri) as k_bri, d.id_akun,
 d.nm_akun, if(d.nominal is null ,0,d.nominal) as nominal , if(d.diskon_bank is null ,0,d.diskon_bank) as diskon_bank
         FROM tb_transaksi as a 
         left join tb_invoice as b on b.no_nota = a.no_order 
@@ -36,7 +36,7 @@ d.nm_akun, if(d.nominal is null ,0,d.nominal) as nominal , if(d.diskon_bank is n
         where c.id_lokasi = ? and c.tgl BETWEEN ? and ?
             group by c.no_order2 ) as c on c.no_order2 = a.no_order 
             left join (
-                SELECT d.no_nota, e.nm_akun, sum(d.nominal) as nominal, sum(d.diskon_bank) as diskon_bank
+                SELECT d.no_nota, e.id_akun, e.nm_akun, sum(d.nominal) as nominal, sum(d.diskon_bank) as diskon_bank
                 FROM pembayaran as d 
                 left join akun_pembayaran as e on e.id_akun_pembayaran = d.id_akun_pembayaran
                 GROUP by d.no_nota
